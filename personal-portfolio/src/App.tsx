066d24da64acc555f60849131288212f99c72c24
anishthebud <abudida3@gatech.edu>
2025-08-22 16:54:14 -0400

    finished adding the pictures for each of the cards (now moving onto each of the pages)

--- personal-portfolio/src/App.tsx
+++ personal-portfolio/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomeCard from './components/HomeCard';
+import HomeCard from './components/HomeCard/HomeCard';
 
 function App() {
   return (
@@ -10,14 +10,14 @@ function App() {
           name="About Me"
           width="320px"
           height="320px"
-          image="https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?q=80&w=1600&auto=format&fit=crop"
+          image="./homeCardPics/aboutMe.png"
           description="Get to know my background, interests, and what drives me."
         />
         <HomeCard
           name="Favorite Films"
           width="320px"
           height="320px"
-          image="https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?q=80&w=1600&auto=format&fit=crop"
+          image="./homeCardPics/favFilms-unsplash.jpg"
           description="Get to know my background, interests, and what drives me."
         />
       </div>
@@ -26,14 +26,14 @@ function App() {
           name="Experience"
           width="720px"
           height="440px"
-          image="https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?q=80&w=1600&auto=format&fit=crop"
+          image="./homeCardPics/experience-unsplash.jpg"
           description="Learn about my experience through projects and college."
         />
         <HomeCard
           name="Resume and Cover Letter"
           width="720px"
           height="200px"
-          image="https://images.unsplash.com/photo-1501785888041-af3ef285b470?q=80&w=2400&auto=format&fit=crop"
+          image="./homeCardPics/resumeCV-unsplash.jpg"
           description="View my latest resume and tailored cover letters."
         />
       </div>
@@ -42,14 +42,14 @@ function App() {
           name="Skills"
           width="320px"
           height="320px"
-          image="https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?q=80&w=1600&auto=format&fit=crop"
+          image="./homeCardPics/skills-unsplash.jpg"
           description="Get to know my background, interests, and what drives me."
         />
         <HomeCard
           name="Current Tech"
           width="320px"
           height="320px"
-          image="https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?q=80&w=1600&auto=format&fit=crop"
+          image="./homeCardPics/currentTech-unsplash.jpg"
           description="Get to know my background, interests, and what drives me."
         />
       </div>
